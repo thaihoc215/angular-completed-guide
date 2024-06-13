b074553eabd1dda142115123d34033354a6d1ce3
hoc.h <hoc.h@kbtgvn.tech>
2024-06-13 15:15:48 +0700

    init custom componenet

--- first-angular-app/src/main.ts
+++ first-angular-app/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'; // main component
+import { HeaderComponent } from './app/header/header.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+
+// index.html load from main.ts get seletor app-root from AppComponent
+
+// bootstrapApplication(HeaderComponent, appConfig).catch((err) => console.error(err));
