f7b41bdeb4692735c9a451777195804e745d096e
hoc.ha <hoc.ha@saigontechnology.com>
2024-06-19 21:35:37 +0700

    add practice investment

---
+++ invest-calculator/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  // @Input() results?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+
+  // user service
+  constructor(private investmentService: InvestmentService) {}
+
+  get results() {
+    return this.investmentService.resultData;
+  }
+}
